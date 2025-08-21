0699a1c2433e847cfd7fed99783e0752b207a6e9
Rooparaj Muthulingam <rooparaj597@gmail.com>
2025-08-21 20:40:12 +0530

    functions

---
+++ Day5/function.py
@@ -0,0 +1,9 @@
+#def is used to declare a function
+#def function_name() whenever you want to use that function use function_name()
+#this will used to decrease the set of lines in a program
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
