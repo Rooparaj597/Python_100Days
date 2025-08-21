5eb796c5a302f51f5aebd43d2319dbc4503e58fc
Rooparaj Muthulingam <rooparaj597@gmail.com>
2025-08-21 20:26:22 +0530

    Operators and datatypes

---
+++ Day2/Typerr.py
@@ -0,0 +1,13 @@
+len("Hello")
+
+print(type("Roopa"))
+
+print(type("hello"))
+print(type(123))
+print(type(3.55))
+print(type(True))
+
+name = input("Enter your name")
+len_name=len(name)
+
+print("Number of letters in your name: " + str(len_name))
---
+++ Day2/num.py
@@ -0,0 +1,16 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+print(round(bmi, 3))
+
+# assignment operator
+
+score = 0
+score+=1
+print(score)
+
+# f String
+
+age = 19
+print(f"My age is {age}.")
